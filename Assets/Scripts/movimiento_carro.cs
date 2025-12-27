9451a9e7e281206c8706dfded9cfb010c284b2de
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2025-12-26 18:38:08 -0600

    Mejora del script de movimiento
    Intente hacer que se muevan las ruedas y que se aplique el giro cuando avance el coche

--- Assets/Scripts/movimiento_carro.cs
+++ Assets/Scripts/movimiento_carro.cs
@@ -1,10 +1,16 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class movimiento_carro : MonoBehaviour
 {
     [Header("General")]
-    [SerializeField] Camera MainCamera;
+    [SerializeField] GameObject ruedas;
+
 
     [Header("Movimiento")]
     [SerializeField] float velocidad = 2.0f;
@@ -26,27 +32,32 @@ public class movimiento_carro : MonoBehaviour
     void Start()
     {
         cController = GetComponent<CharacterController>();
-
+    
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        float currentSpeed = velocidad;
-
         Vector2 mVector = Move.action.ReadValue<Vector2>();
 
         float moviminetoVertical = mVector.y;
         float moviminetoHorizontal = mVector.x;
 
-        transform.Rotate(0, mVector.x * velocidadGiro * Time.deltaTime, 0);
-
-        if(Mathf.Abs(moviminetoVertical) > 0.01f)
+        if(Mathf.Abs(moviminetoHorizontal)> 0.01f)
         {
-            Vector3 movimiento = transform.forward * moviminetoVertical * velocidad;
+
+            foreach (Transform rueda in ruedas.GetComponentsInChildren<Transform>())
+            {
+                rueda.transform.Rotate(0,moviminetoHorizontal * Time.deltaTime, 0);
+            }
+        }
+
+        if (Mathf.Abs(moviminetoVertical) > 0.01f)
+        {
+            Vector3 movimiento = moviminetoVertical * velocidad * transform.forward;
+
             cController.Move(movimiento * Time.deltaTime);
             
         }
