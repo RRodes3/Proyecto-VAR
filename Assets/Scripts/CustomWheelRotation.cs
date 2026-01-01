7d0b8d30800ce63d20eb8b68c489cdab8cd93a56
Rand0m-Guy <pabloisaacmacias@gmail.com>
2026-01-01 02:06:20 -0600

    Funciona volante

--- Assets/Scripts/CustomWheelRotation.cs
+++ Assets/Scripts/CustomWheelRotation.cs
@@ -8,21 +8,22 @@ using UnityEngine.InputSystem;
 public class CustomWheelRotation : MonoBehaviour
 {
 
+    [SerializeField] Transform wheelCenter;
     [SerializeField] Transform rotationReference;
     [SerializeField] float epsilon = 0.0001f;
-
+    
     void Update() {
         if(rotationReference.parent != null) return; // Objeto no está agarrado
         
-        Vector3 OA = transform.position;
-        Vector3 OB = rotationReference.position;
+        Vector3 OV = wheelCenter.transform.position;
+        Vector3 OR = rotationReference.position;
+        Vector3 VR = OR - OV;
 
-        Vector3 AB = OB - OA;
-        float alpha = Mathf.Atan(AB.x / AB.y);
+        float finalRot = Mathf.Atan2(VR.y, VR.x) * Mathf.Rad2Deg; // Entre -180 y 180 grados
+        finalRot = -finalRot; // Mi rotación local va al revés
+        if(finalRot <= 0f) finalRot += 360f;
+        if(finalRot >= 360f) finalRot -= 360f;
 
-        if(alpha * Mathf.Rad2Deg <= epsilon) return; // Llegó
-
-        Debug.Log("Alpha: " + alpha);
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + alpha * Mathf.Rad2Deg, transform.localEulerAngles.z);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, finalRot, transform.localEulerAngles.z);
     }
 }
