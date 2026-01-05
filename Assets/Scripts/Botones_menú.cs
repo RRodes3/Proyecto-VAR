c2e859d959feb5b0fd0b4af40e677f396af3d6c4
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-01-04 21:55:33 -0600

    Menú principal
    Todo cutre pero ya cambia de escenas con los mandos del vr, el de salir en teoria funciona pero ya cuando se corre la aplicación en los vr.

---
+++ Assets/Scripts/Botones_menú.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Botonesmenú : MonoBehaviour
+{
+    [Header("Canvas")]
+    [SerializeField] GameObject principal;
+    [SerializeField] GameObject niveles;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void ElegirNivel()
+    {
+        principal.SetActive(false);
+        niveles.SetActive(true);
+    }
+
+    public void Semaforo()
+    {
+        SceneManager.LoadScene("Juego_Semaforo");
+    }
+
+    public void Carriles()
+    {
+        SceneManager.LoadScene("Juego_CedaElPaso");
+    }
+
+    public void SeñalAlto()
+    {
+        SceneManager.LoadScene("Juego_Alto");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
